31ebac212b4b453b421d0598a58522c1fcda87fa
Joseph Mchaileh <Joseph.Mchaileh@wtc.com.au>
2019-01-16 12:01:28 +1100

    updated output

--- app.py
+++ app.py
@@ -1,13 +1,35 @@
+# -*- coding: utf-8 -*-
+import dash
+import dash_core_components as dcc
+import dash_html_components as html
 from flask import Flask
-import plotly.plotly as py
-import plotly.graph_objs as go
-app = Flask(__name__)
 
-@app.route("/")
-def plot():
-	data = [go.Bar(
-            x=['giraffes', 'orangutans', 'monkeys'],
-            y=[20, 14, 23]
-    	)]
-	py.iplot(data, filename='basic-bar')
-    
+server = Flask(__name__)
+
+external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
+
+app = dash.Dash(__name__, external_stylesheets=external_stylesheets,server=server)
+
+app.layout = html.Div(children=[
+    html.H1(children='Hello Dash'),
+
+    html.Div(children='''
+        Dash: A web application framework for Python.
+    '''),
+
+    dcc.Graph(
+        id='example-graph',
+        figure={
+            'data': [
+                {'x': [1, 2, 3], 'y': [4, 1, 2], 'type': 'bar', 'name': 'SF'},
+                {'x': [1, 2, 3], 'y': [2, 4, 5], 'type': 'bar', 'name': u'Montréal'},
+            ],
+            'layout': {
+                'title': 'Dash Data Visualization'
+            }
+        }
+    )
+])
+
+if __name__ == '__main__':
+    app.run_server(debug=True)
